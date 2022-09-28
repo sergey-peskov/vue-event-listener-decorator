893d53dae43cf4e1719c800b057f6a56277621f8
Sergey <messi31081995@gmail.com>
2022-09-28 15:00:33 +0300

    context fix

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,28 @@
 import { createDecorator } from 'vue-class-component';
 
+type EventListener = (e: Event) => void
+
 export function VueEventListenerDecorator<
 	TTarget extends Window | Document,
 	TEventName extends TTarget extends Window ? keyof WindowEventMap : keyof DocumentEventMap
 >(target: TTarget, eventName: TEventName, options?: AddEventListenerOptions) {
 	return createDecorator((componentOptions, handler) => {
-		let handlerFn = componentOptions.methods![handler];
-		if (typeof handlerFn !== 'function') {
+		if (typeof componentOptions.methods![handler] !== 'function') {
 			throw new TypeError('watch handler is not a function');
 		}
 		let mounted = componentOptions.mounted;
 		let beforeDestroy = componentOptions.beforeDestroy;
+		let handlerFn: null| EventListener = null;
 		componentOptions.mounted = function () {
+			handlerFn = (this as any)[handler] as EventListener;
 			target.addEventListener(eventName, handlerFn, options);
 			mounted?.call(this);
 		};
 
 		componentOptions.beforeDestroy = function () {
-			target.removeEventListener(eventName, handlerFn, options);
+			if(handlerFn) {
+				target.removeEventListener(eventName, handlerFn, options);
+			}
 			beforeDestroy?.call(this);
 		};
 	});
